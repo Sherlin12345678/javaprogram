2f077f4ef5e41a24e16f3f02b6c90d95a87ac6ec
Sherlin12345678 <112100740+Sherlin12345678@users.noreply.github.com>
2024-07-31 12:12:31 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
-public class Hello
-{
-    public static void main(String args[])
-    {
-        System.out.println("WElcome to DEvops lab");
-        }
-      }
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
